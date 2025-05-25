74b5e20219617c04f13121016a9087fa5edc3885
ilya-pishchalnikov <ilya_chaik@mail.ru>
2025-05-25 06:31:49 +0300

    Add the capability to delete users via the CLI

--- store/users.go
+++ store/users.go
@@ -48,6 +48,62 @@ func InsertUser(db *sql.DB, user User) error {
 	return err
 }
 
+func UpsertUser(db *sql.DB, user User) error {
+	var exists bool
+	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM user WHERE user_id = ?)", user.UserId).Scan(&exists)
+
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		_, err = db.Exec(`
+		UPDATE user 
+		SET 
+			 name = ?
+		   , login = ?
+		   , password_hash = ? 
+		WHERE user_id = ?`,
+			user.Name, user.Login, user.PasswordHash, user.UserId)
+
+		return err
+	} else {
+
+		err = db.QueryRow("SELECT EXISTS(SELECT 1 FROM user WHERE login = ?)", user.Login).Scan(&exists)
+
+		if err != nil {
+			return err
+		}
+
+		if exists {
+			return errors.New("A login '" + user.Login + "' is already registered")
+		}
+
+		_, err = db.Exec(`
+		INSERT INTO user (user_id, name, login, password_hash) 
+		VALUES (?, ?, ?, ?)`,
+			user.UserId, user.Name, user.Login, user.PasswordHash)
+
+		return err
+	}
+}
+
+func DeleteUser(db *sql.DB, userId string) error {
+	var exists bool
+	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM user WHERE user_id = ?)", userId).Scan(&exists)
+
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		return errors.New("A user with ID '" + userId + "' is not registered")
+	}
+
+	_, err = db.Exec(`DELETE FROM user WHERE user_id = ?`, userId)
+
+	return err
+}
 func GetUserPasswordHashByLogin(db *sql.DB, login string) (string, error) {
 	var exists int
 	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM user WHERE login = ?)", login).Scan(&exists)
@@ -64,7 +120,16 @@ func GetUserPasswordHashByLogin(db *sql.DB, login string) (string, error) {
 }
 
 func GetUserIdByLogin(db *sql.DB, login string) (string, error) {
+	var exists bool
+	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM user WHERE login = ?)", login).Scan(&exists)
+	if err != nil {
+		return "", err
+	}
+	if !exists {
+		return "", nil
+	}
+
 	var userId string
-	err := db.QueryRow("SELECT user_id FROM user WHERE login = ?", login).Scan(&userId)
+	err = db.QueryRow("SELECT user_id FROM user WHERE login = ?", login).Scan(&userId)
 	return userId, err
 }
--- todopp.go
+++ todopp.go
@@ -11,23 +11,26 @@ import (
 )
 
 func main() {
-	// command line attributes variables
+	appConfig, err := util.GetConfig()
 
-	var userSet *bool
-	var userName *string
-	var userLogin *string
-	var userPassword *string
+	// // command line attributes variables
+	// var userSet *bool
+	// var userDelete *bool
+	// var userName *string
+	// var userLogin *string
+	// var userPassword *string
 
 	// parse command line attributes
 	if len(os.Args) > 0 {
-		userSet = flag.Bool("user", false, "Input user data mode")
-		userName = flag.String("name", "", "User name")
-		userLogin = flag.String("login", "", "User login")
-		userPassword = flag.String("password", "", "User password")
+		userSet := flag.Bool("user", false, "Input user data mode")
+		userDelete := flag.Bool("delete", false, "Input user data mode")
+		userName := flag.String("name", "", "User name")
+		userLogin := flag.String("login", "", "User login")
+		userPassword := flag.String("password", "", "User password")
 
 		flag.Parse()
 
-		if userSet != nil && *userSet {
+		if userSet != nil && *userSet && (userDelete == nil || !*userDelete) {
 			if userName == nil || *userName == "" {
 				fmt.Println("User name must be defined")
 			}
@@ -38,38 +41,73 @@ func main() {
 				fmt.Println("User password must be defined")
 			}
 		}
-	}
 
-	appConfig, err := util.GetConfig()
-
-	if userSet != nil && *userSet {
-		db, err := store.OpenDb(appConfig.DbPath)
-		if err != nil {
-			fmt.Print("Error opening database: ", err)
-			log.Fatal(err)
-		}
-		defer db.Close()
-
-		passwordHash, err := util.HashPassword(*userPassword)
-		if err != nil {
-			fmt.Print("Error hashing password: ", err)
-			log.Fatal(err)
+		if userSet != nil && *userSet && userDelete != nil && *userDelete {
+			if userLogin == nil || *userLogin == "" {
+				fmt.Println("User login must be defined")
+			}
 		}
 
-		var user store.User
+		if userSet != nil && *userSet && (userDelete == nil || !*userDelete) {
+			db, err := store.OpenDb(appConfig.DbPath)
+			if err != nil {
+				fmt.Print("Error opening database: ", err)
+				log.Fatal(err)
+			}
+			defer db.Close()
 
-		user.UserId = util.Uuid()
-		user.Name = *userName
-		user.Login = *userLogin
-		user.PasswordHash = passwordHash
+			passwordHash, err := util.HashPassword(*userPassword)
+			if err != nil {
+				fmt.Print("Error hashing password: ", err)
+				log.Fatal(err)
+			}
 
-		err = store.InsertUser(db, user)
-		if err != nil {
-			fmt.Print("Error storing user: ", err)
-			log.Fatal(err)
+			userId, err := store.GetUserIdByLogin(db, *userLogin)
+			if err != nil {
+				fmt.Print("Error getting user id by login: ", err)
+				log.Fatal(err)
+			}
+			var user store.User
+
+			if userId != "" {
+				user.UserId = userId
+			} else {
+				user.UserId = util.Uuid()
+			}
+			user.Name = *userName
+			user.Login = *userLogin
+			user.PasswordHash = passwordHash
+
+			err = store.UpsertUser(db, user)
+			if err != nil {
+				fmt.Print("Error storing user: ", err)
+				log.Fatal(err)
+			}
+
+			return
 		}
 
-		return
+		if userSet != nil && *userSet && userDelete != nil && *userDelete {
+			db, err := store.OpenDb(appConfig.DbPath)
+			if err != nil {
+				fmt.Print("Error opening database: ", err)
+				log.Fatal(err)
+			}
+			defer db.Close()
+
+			userId, err := store.GetUserIdByLogin(db, *userLogin)
+			if err != nil {
+				fmt.Print("Error getting user id by login: ", err)
+				log.Fatal(err)
+			}
+
+			err = store.DeleteUser(db, userId)
+			if err != nil {
+				fmt.Print("Error deleting user: ", err)
+				log.Fatal(err)
+			}
+			return
+		}
 	}
 
 	err = store.InitDatabase(appConfig.DbPath)
